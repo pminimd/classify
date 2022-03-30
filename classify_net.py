498cfada6d546b69502f5bba339f0f736810a90d
pminimd <80637505+pminimd@users.noreply.github.com>
2022-03-30 18:05:41 +0800

    Create classify_net.py

---
+++ classify_net.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+class Mura_net(nn.Module):
+
+    def __init__(self):
+
+        super(Mura_net, self).__init__()
+
+        self.first_layer = nn.Conv2d(3,6,kernel_size=3,padding=1)
+        self.prelu1 = nn.PReLU()
+        self.depthwise_layer = nn.Conv2d(6,6,kernel_size=3,padding=1,groups=6)
+        self.pointwise_layer = nn.Conv2d(6,2,kernel_size=1)
+        self.prelu2 = nn.PReLU()
+        self.middle_layer = nn.Conv2d(2,2,kernel_size=3,padding=1,stride=2)
+        self.last_layer1 = nn.Conv2d(2,1,kernel_size=3,padding=1,stride=2)
+        self.last_layer2 = nn.Conv2d(1,1,kernel_size=3,padding=1,stride=2)
+        self.Linear_1 = nn.Linear(2,1)
+        # self.Linear_2 = nn.Linear(1,1)
+    
+    def forward(self, x):
+        x = self.first_layer(x)
+        # print(x.shape)
+        x = self.prelu1(x)
+        x = self.depthwise_layer(x)
+        x = self.pointwise_layer(x)
+        # print(x.shape)
+        x = self.prelu2(x)
+        x = self.middle_layer(x)
+        x = self.last_layer1(x)
+        x = self.last_layer2(x)
+        print(x.shape)
+        x = self.Linear_1(x)
+        # print(x.shape)
+        # x = self.Linear_2(x)
+        print(x.shape)
+        return F.log_softmax(x, dim=3)
+
+if __name__ == '__main__':
+    mura_net = Mura_net()
+    input_x = torch.rand(1, 3, 12, 12)
+    mura_net.forward(input_x)
+
+    torch.save(mura_net, 'model.pth')
+    model = torch.load('model.pth')
+    model.eval()
+    model_path = "test.onnx"
+
+    torch.onnx.export(model,input_x,model_path,opset_version=10,input_names=["input"],output_names=["output"],dynamic_axes={"input":{0:"batch_size"},"output":{0:"batch_size"}})
