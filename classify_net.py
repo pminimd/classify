474bed6a2da9044255f816ec37bd65df8f9f461e
pminimd <80637505+pminimd@users.noreply.github.com>
2022-04-02 18:04:31 +0800

    Update classify_net.py

--- classify_net.py
+++ classify_net.py
@@ -7,7 +7,7 @@ class Mura_net(nn.Module):
 
     def __init__(self):
 
-        super(Mura_net, self).__init__()
+        super().__init__()
 
         self.first_layer = nn.Conv2d(3,6,kernel_size=3,padding=1)
         self.prelu1 = nn.PReLU()
@@ -17,7 +17,7 @@ class Mura_net(nn.Module):
         self.middle_layer = nn.Conv2d(2,2,kernel_size=3,padding=1,stride=2)
         self.last_layer1 = nn.Conv2d(2,1,kernel_size=3,padding=1,stride=2)
         self.last_layer2 = nn.Conv2d(1,1,kernel_size=3,padding=1,stride=2)
-        self.Linear_1 = nn.Linear(2,1)
+        self.Linear_1 = nn.Linear(4,2)
         # self.Linear_2 = nn.Linear(1,1)
     
     def forward(self, x):
@@ -31,21 +31,24 @@ class Mura_net(nn.Module):
         x = self.middle_layer(x)
         x = self.last_layer1(x)
         x = self.last_layer2(x)
-        print(x.shape)
-        x = self.Linear_1(x)
+        # print(x.shape)
+        x = self.Linear_1(x.view(-1, 4))
         # print(x.shape)
         # x = self.Linear_2(x)
-        print(x.shape)
-        return F.log_softmax(x, dim=3)
+        # print(x)
+        return x
 
 if __name__ == '__main__':
     mura_net = Mura_net()
     input_x = torch.rand(1, 3, 12, 12)
-    mura_net.forward(input_x)
+    output = mura_net.forward(input_x)
+    
+    print(output.shape)
+    print(output)
 
     torch.save(mura_net, 'model.pth')
-    model = torch.load('model.pth')
+    model = torch.load('/home/wenjun/Documents/检测网络/mura_net/995loss0.0023732536064926535mura_net.pth')
     model.eval()
-    model_path = "test.onnx"
+    model_path = "test_95.onnx"
 
     torch.onnx.export(model,input_x,model_path,opset_version=10,input_names=["input"],output_names=["output"],dynamic_axes={"input":{0:"batch_size"},"output":{0:"batch_size"}})
